59658c219ef557555f620e665ab2a7454981d2e0
Austin Schroeder <austin@Andrews-iPhone.local>
2021-05-08 11:30:13 -0700

    Server is setup.  Just starting to add paths and views.

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
-const PORT = 4000
+const PORT = 4000;
 
+
+app.set('view engine', 'ejs');
+
+
+
+
+
+//-----SPLASH(home) Route-----
 app.get('/', (req, res) => {
-    res.send('We have Cheese!')
+    res.render('index.ejs');
+});
+
+
+
+//-----INDEX(brands) Route-----
+app.get('/brands', (req, res) => {
+    res.render('brands/brandsIndex');
 })
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
-    console.log(`Connected at ${PORT}`)
-})
+    console.log(`Connected at port: ${PORT}  YOU GOT THIS!`)
+});
 
 
 
