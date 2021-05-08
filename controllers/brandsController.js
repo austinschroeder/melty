bf7539f8abbc22a13946b6c9e1011a680f18fe48
Austin Schroeder <austin@Andrews-iPhone.local>
2021-05-08 12:15:41 -0700

    added controllers and form for create page.  starting to add post routes and middleware

---
+++ controllers/brandsController.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+//////////////////////////////////////////
+//CURRENT ROUTE - /brands
+//////////////////////////////////////////
+
+
+//Show list of all brands ROUTE(Index)
+router.get('/', (req, res) => {
+    res.render('brands/brandsIndex');
+});
+
+
+//Show a form to add new brand ROUTE
+router.get('/new', (req, res) => {
+    res.render('brands/brandsNew')
+});  
+
+
+//Handle POST request to add NEW BRAND (from brandsNew.ejs form)
+router.post('/', (req, res) => {
+    // - GET from data from request body(brandsNew.ejs)
+
+
+    // - Redirect back to brands Index page AFTER submitting
+    res.redirect('/brands');
+});
+
+
+
+//EXPORT to server.js
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+//Import brandsController
+const brandsController = require('./controllers/brandsController')
 const app = express();
 const PORT = 4000;
 
-
+//CONFIG//
 app.set('view engine', 'ejs');
 
+//MIDWARE// (allow access to request body, (parse request body))
+app.use(express.urlencoded({ extended: false}));
 
-
+//For all requests to /brands, send to brandsController
+app.use('/brands', brandsController);
 
 
 //-----SPLASH(home) Route-----
@@ -16,10 +21,12 @@ app.get('/', (req, res) => {
 
 
 
-//-----INDEX(brands) Route-----
-app.get('/brands', (req, res) => {
-    res.render('brands/brandsIndex');
-})
+// //-----INDEX(brands) Route-----
+// app.get('/brands', (req, res) => {
+//     res.render('brands/brandsIndex');
+// });
+
+
 
 
 
